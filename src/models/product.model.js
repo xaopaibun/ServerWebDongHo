7b9e79cee856b7bc1df739a80c097a55f7962799
xaopaibun <vanquy33338888@gmail.com>
2021-09-06 17:45:07 +0700

    update content default

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 let ProductSchema = new Schema({
     name: String,
-    content: String,
+    content: {type : String, default: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"},
     amount: { type: Number, default: 1 },
     image: String,
     price : { type: Number, default: 1000000 },
